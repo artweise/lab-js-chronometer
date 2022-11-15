25e32b60237e048a614cd9f7fb8aa59c0f97b054
artweise <art2wsxweise7ujm@gmail.com>
2022-11-15 07:02:30 +0100

    done all iterations except the bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,40 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (typeof printTimeCallback === "function") {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let newValue = value.toString();
+    if (value < 10) {
+      newValue = newValue.padStart(2, "0");
+    }
+    return newValue;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
